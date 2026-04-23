1a095d2c6bc1ea0530946c4523395e5bab00baf3
giangt1999-TVG <giangt1999@gmail.com>
2026-04-23 22:53:08 +0700

    add connect Mongo DB

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express =  require("express");
+const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Get infor');
+    res.send('Get infor Hello G');
 });
 
 app.listen(3000, (req, res) => {
     console.log('Node api running on port 3000');
+});
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb+srv://giangt1999_db_user:BOUVTvQZXBHNKGIo@giangtongapi.qq09u5c.mongodb.net/?appName=GiangTongAPI')
+.then(() => {
+    console.log('Connect mongo database');
+}).catch((error) => {
+    console.log(error);
 });
